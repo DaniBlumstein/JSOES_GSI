ad9319d7297eb6080800326af9257983ed58b75a
Dani Blumstein <49007024+DaniBlumstein@users.noreply.github.com>
2025-03-21 09:16:57 -0700

    Delete jsoes_pbt.sh

--- jsoes_pbt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --job-name=jsoes_pbt
-#SBATCH -o jsoes_pbt.out
-#SBATCH -e jsoes_pbt.err
-#SBACH -n 20
-
-/nfs5/FW_HMSC/Omalley_Lab/snppit/snppit-Linux -f May2024_PBT_Input.txt
